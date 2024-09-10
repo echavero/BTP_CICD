8b4158242d0255cc422e7108f89da0dfec09c066
echavero <esther_chavero@yahoo.com.mx>
2024-09-10 04:45:38 +0000

    CI/CD

---
+++ project_fiori/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("logaligroup.projectfiori.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
